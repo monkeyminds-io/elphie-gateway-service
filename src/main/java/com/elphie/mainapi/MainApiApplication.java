97e1a05378f70d6dce99d5f814483ddebc53e9a6
Pau Ferrer <MY_NAME@example.compau-ferrer@minimaldesigns.ie>
2024-01-08 10:32:35 +0000

    first commit

---
+++ src/main/java/com/elphie/mainapi/MainApiApplication.java
@@ -0,0 +1,13 @@
+package com.elphie.mainapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elphie/mainapi/MainApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elphie.mainapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
